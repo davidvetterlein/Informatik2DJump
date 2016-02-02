a1cfa9285e14fc551eee56c0086633acc4cd303f
josias conradi <green.puma@web.de>
2016-02-02 13:01:14 +0100

    no message

---
+++ InformatikProjekt/Assets/scripte/steuerung.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class steuerung : MonoBehaviour {
+	public bool rechts=false;
+	public bool links=false;
+	public float speed=0f;
+
+	public void clickupL (){
+		links = false;
+		
+	}
+	public void clickdownL (){
+		links = true;
+		
+	}
+	public void clickupR (){
+		rechts = false;
+
+	}
+	public void clickdownR (){
+		rechts = true;
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rechts == true) {
+			gameObject.transform.position += new Vector3 (speed, 0, 0);
+		}
+		if (links == true) {
+			gameObject.transform.position -= new Vector3 (speed, 0, 0);
+		}
+	
+	}
+}
