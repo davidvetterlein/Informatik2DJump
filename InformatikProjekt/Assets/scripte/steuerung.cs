6480d2bf7943c77df131e47c9621934b6998f7a6
David Vetterlein <jdv5876@gmail.com>
2016-02-09 11:00:17 +0100

    no message

--- InformatikProjekt/Assets/scripte/steuerung.cs
+++ InformatikProjekt/Assets/scripte/steuerung.cs
@@ -5,6 +5,10 @@ public class steuerung : MonoBehaviour {
 	public bool rechts=false;
 	public bool links=false;
 	public float speed=0f;
+	public bool jumping = false;
+	public int jump = 50;
+	public int newjump = 50;
+	public float up;
 
 	public void clickupL (){
 		links = false;
@@ -22,6 +26,13 @@ public class steuerung : MonoBehaviour {
 		rechts = true;
 		
 	}
+	
+	public void Jump () {
+		if(jump == 0){
+			jumping = true;
+			jump = newjump;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -30,6 +41,14 @@ public class steuerung : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (jumping == true && jump >= 1){
+			jump -= 1;
+			gameObject.GetComponent<Rigidbody>().Sleep();
+			gameObject.transform.position += new Vector3 (0, up, 0);
+		} else if (jumping == true && jump <= 0){
+			gameObject.GetComponent<Rigidbody>().WakeUp();
+			jumping = false;
+		}
 		if (rechts == true) {
 			gameObject.transform.position += new Vector3 (speed, 0, 0);
 		}
